072420b0fed1528fe3757a4f2cd51eba3dae9f06
Mudasiru Abdulsalam <108347465+tosvn@users.noreply.github.com>
2023-08-05 09:32:06 -0700

    Update 1-edges.py

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-a = (word[:3])
-b = (word[-1:])
-c = (word[4])
+word_first_3 = (word[:3])
+word_last_2 = (word[-1:])
+middle_word = (word[4])
 print("First 3 letters: {a}".format(word_first_3))
 print("Last 2 letters: {b}".format(word_last_2))
 print("Middle word: {c}".format(middle_word))
