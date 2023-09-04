345aaeee3dadea7ddf37d83a1b1664f3ba68a8e7
Abdulsalam <abdulsalammudasiru2021@gmail.com>
2023-09-04 15:32:34 -0700

    2-temperature

---
+++ python-functions/2-temperature.py
@@ -0,0 +1,13 @@
+def convert_to_celsius(fahrenheit):
+    return celsius 
+
+
+
+
+
+
+if __name__ = "__main__":
+    print(convert_to_celsius(100))
+    print(convert_to_celsius(-40))
+    print(convert_to_celsius(-459.67))
+    print(convert_to_celsius(32))
