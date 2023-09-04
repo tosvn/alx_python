dcff670974b74b83d1bf6c2e879a756f06efa59d
Abdulsalam <abdulsalammudasiru2021@gmail.com>
2023-09-04 15:34:48 -0700

    corrected task 2

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,13 +1,3 @@
 def convert_to_celsius(fahrenheit):
-    return celsius 
-
-
-
-
-
-
-if __name__ = "__main__":
-    print(convert_to_celsius(100))
-    print(convert_to_celsius(-40))
-    print(convert_to_celsius(-459.67))
-    print(convert_to_celsius(32))
+    celsius = (fahrenheit - 32) * 5 / 9
+    return celsius
