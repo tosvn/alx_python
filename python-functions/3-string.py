02313e4ce1329ccb3f1f26eb849cfdc56b7b397d
Abdulsalam <abdulsalammudasiru2021@gmail.com>
2023-09-04 15:46:57 -0700

    3-string

---
+++ python-functions/3-string.py
@@ -0,0 +1,10 @@
+def reverse_string(string):
+    return string[::-1]
+
+
+
+if __name__ = "__main__":
+    print(reverse_string("Hello"))
+    print(reverse_string(""))
+    print(reverse_string("madam"))
+    print(reverse_string("Hello, World!"))  
