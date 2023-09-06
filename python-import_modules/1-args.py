4f24fe03d4c2d1ef99377f17149d7821740288af
Abdulsalam <abdulsalammudasiru2021@gmail.com>
2023-09-06 06:05:04 -0700

    1-args

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,12 @@
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    num_args = len(args)
+
+    if num_args == 0:
+        print("0 arguments.")
+    else:
+        print(f"{num_args} argument{'s' if num_args > 1 else ''}:")
+        for i, arg in enumerate(args, 1):
+            print(f"{i}: {arg}")
